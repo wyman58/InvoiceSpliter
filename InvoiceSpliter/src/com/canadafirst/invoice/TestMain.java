17096eefa2edf785b5ce8396c9f6c863e5eb7e66
Wyman Chen <wyman.58@gmail.com>
2015-03-02 22:37:14 +1000

    Test push

---
+++ InvoiceSpliter/src/com/canadafirst/invoice/TestMain.java
@@ -0,0 +1,12 @@
+package com.canadafirst.invoice;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test GitHub");
+		System.out.println("Test push");
+
+	}
+
+}
